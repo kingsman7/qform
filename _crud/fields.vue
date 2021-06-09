1de3e0fca9d6e45714a23861dfd574d2c47856fb
msolanogithub <msolano2505@gmail.com>
2021-06-09 17:04:11 -0500

    +Add: qform | field `selectIcon` to field CRUD

--- _crud/fields.vue
+++ _crud/fields.vue
@@ -137,6 +137,11 @@ export default {
           },
         },
         formRight: {
+          icon: {
+            value: null,
+            type: 'selectIcon',
+            isFakeField: true
+          },
           prefixType: {
             name: 'type',
             value: '',
@@ -205,19 +210,19 @@ export default {
       this.loading = true
       let params = {params: {}}
       this.$crud.index('apiRoutes.qform.forms', params)
-        .then(response => {
-          this.forms = response.data.map(item => {
-            return {
-              id: item.id,
-              label: item.title
-            }
+          .then(response => {
+            this.forms = response.data.map(item => {
+              return {
+                id: item.id,
+                label: item.title
+              }
+            })
+            this.loading = false
           })
-          this.loading = false
-        })
-        .catch(error => {
-          this.$alert.error({message: this.$tr('ui.message.errorRequest'), pos: 'bottom'})
-          this.loading = false
-        })
+          .catch(error => {
+            this.$alert.error({message: this.$tr('ui.message.errorRequest'), pos: 'bottom'})
+            this.loading = false
+          })
     },
   },
 }
